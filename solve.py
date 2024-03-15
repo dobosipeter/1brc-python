b922ef8bcb9c817a541449c34581260dafbd234b
Peter Dobosi <dobosipeter96@gmail.com>
2024-03-16 00:27:26 +0100

    Added initial module

---
+++ solve.py
@@ -0,0 +1,8 @@
+"""This module contains my attempt at solving the challenge."""
+
+def main():
+    print("Jk, I'm setting up neovim for python development first.")
+
+if __name__ == "__main__":
+    main()
+
